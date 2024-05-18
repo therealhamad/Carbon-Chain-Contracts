875a0c64f178d6905fab06caa55a76f8b8cd5f61
sahil-raj <sahilrajaaru@gmail.com>
2024-05-18 16:56:46 +0530

    buyReq

--- contracts/CarbonChain.sol
+++ contracts/CarbonChain.sol
@@ -31,6 +31,18 @@ contract CarbonChain is ERC20 {
         _;
     }
 
+    modifier buyReq(uint256 _amount, address _buyFrom) {
+        require(msg.value >= _amount*creditRate(), "more amount required");
+        bool mmt = false;
+        for (uint i=0; i<toSellArr.length; i++) {
+            if (toSellArr[i].sellerAddress == _buyFrom) {
+                mmt = true;
+            }
+        }
+        require(mmt, "There must be the specified seller");
+        _;
+    }
+
     function creditRate() public returns(uint256) {
         return ((creditValue*initialAmount)/totalSupply());
     }
@@ -38,9 +50,12 @@ contract CarbonChain is ERC20 {
     function sellCredit(uint256 _sellAmount) public sellReq(_sellAmount) returns(bool) {
         toSell memory x = toSell(_sellAmount, msg.sender);
         toSellArr.push(x);
+        return true;
     }
 
-    function buyCredit() {}
+    function buyCredit(uint256 _amount, address _buyFrom) public payable buyReq(_amount, _buyFrom) returns(bool) {
+        (bool success, ) = forgMapping[_eventId].creator.call{value: forgMapping[_eventId].ticket_price*_ticketCount}("");
+    }
 
     function generateCredits() {
 

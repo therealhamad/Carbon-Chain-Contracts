f0958bdcd558fe29d281c973e303d7970e80d98c
sahil-raj <sahilrajaaru@gmail.com>
2024-05-18 15:03:11 +0530

    changes

--- contracts/CarbonChain.sol
+++ contracts/CarbonChain.sol
@@ -1,31 +1,49 @@
 //contracts/CarbonChain.sol
-//SPDX-Licensce-Identifier: MIT
+//SPDX-License-Identifier: GPL-3.0-or-later
 
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract CarbonChain is ERC20 {
-    
-    constructor (uint256 _initialSupply, uint256 _upperLimit) ERC20("CarbonCredits", "CCT") {
 
-        require(_initialSupply<=_upperLimit, "Upper limit cannot be more than total supply");
-        //supply all the initial tokens to the deployer
-        _mint(msg.sender, _initialSupply);
+    uint256 public creditValue;
+    uint256 public initialAmount;
+
+    struct toSell{
+        uint256 amountToSell;
+        address sellerAddress;
     }
 
-    // mapping(address => uint256) public creditAccountsBalances;
+    toSell[] toSellArr;
 
-    mapping(address => uint256) private toSell;
+    constructor(uint256 _initialAmount, uint256 _maxLimit) ERC20("CarbonCredit", "CCT"){
+        require(_initialAmount <= _maxLimit, "Max amount must be less than initial value");
+        initialAmount = _initialAmount;
+        //supply all the tokens to the creator of the contract
+        _mint(msg.sender, _initialAmount);
+        //set one credit equal to one dollar in eth
+        creditValue = 32*10**18;
+    }
 
-    modifier sellMod(uint256 _amount) {
-        require(_amount <= balanceOf(msg.sender), "cannot sell more than possesion");
+    modifier sellReq(uint256 _sellAmount) {
+        require(balanceOf(msg.sender) >= _sellAmount, "Cannot sell more than possesion");
         _;
     }
 
-    function sellCredits(uint256 _amount) public sellMod(_amount) returns(bool) {
-        // creditAccountsBalances[msg.sender] -= _amount;
-        toSell[msg.sender] += _amount;
-        return true;
+    function creditRate() public returns(uint256) {
+        return ((creditValue*initialAmount)/totalSupply());
     }
+
+    function sellCredit(uint256 _sellAmount) public sellReq(_sellAmount) returns(bool) {
+        toSell memory x = toSell(_sellAmount, msg.sender);
+        toSellArr.push(x);
+    }
+
+    function buyCredit() {}
+
+    function generateCredits() {
+
+    }
+
 }

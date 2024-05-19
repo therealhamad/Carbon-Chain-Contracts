117e929266f0339b48daa3b40382ee3734c52b7e
sahil-raj <sahilrajaaru@gmail.com>
2024-05-19 09:34:48 +0530

    polygon depolyment

--- contracts/CarbonChain.sol
+++ contracts/CarbonChain.sol
@@ -3,40 +3,50 @@
 
 //sepolia: 0xbA6D779Ebf3EADA6c805c29215751004dBDa46ef
 
+//polygon: 0xF03c83b0fF20D5D5A13A15Fa8b0BA66385E9c91d
+
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract CarbonChain is ERC20 {
-
     uint256 public creditValue;
     uint256 public initialAmount;
 
-    struct toSell{
+    struct toSell {
         uint256 amountToSell;
         address sellerAddress;
     }
 
     toSell[] toSellArr;
 
-    constructor(uint256 _initialAmount, uint256 _maxLimit) ERC20("CarbonCredit", "CCT"){
-        require(_initialAmount <= _maxLimit, "Max amount must be less than initial value");
+    constructor(
+        uint256 _initialAmount,
+        uint256 _maxLimit
+    ) ERC20("CarbonCredit", "CCT") {
+        require(
+            _initialAmount <= _maxLimit,
+            "Max amount must be less than initial value"
+        );
         initialAmount = _initialAmount;
         //supply all the tokens to the creator of the contract
-        _mint(msg.sender, _initialAmount * (10**decimals()));
+        _mint(msg.sender, _initialAmount * (10 ** decimals()));
         //set one credit equal to one dollar in eth
-        creditValue = 32*(10**decimals());
+        creditValue = 32 * (10 ** decimals());
     }
 
     modifier sellReq(uint256 _sellAmount) {
-        require(balanceOf(msg.sender) >= _sellAmount, "Cannot sell more than possesion");
+        require(
+            balanceOf(msg.sender) >= _sellAmount,
+            "Cannot sell more than possesion"
+        );
         _;
     }
 
     modifier buyReq(uint256 _amount, address _buyFrom) {
-        require(msg.value >= _amount*creditRate(), "more amount required");
+        require(msg.value >= _amount * creditRate(), "more amount required");
         bool mmt = false;
-        for (uint i=0; i<toSellArr.length; i++) {
+        for (uint i = 0; i < toSellArr.length; i++) {
             if (toSellArr[i].sellerAddress == _buyFrom) {
                 mmt = true;
             }
@@ -49,35 +59,42 @@ contract CarbonChain is ERC20 {
         _;
     }
 
-    function creditRate() public returns(uint256) {
-        return ((creditValue*initialAmount)/totalSupply());
+    function creditRate() public returns (uint256) {
+        return ((creditValue * initialAmount) / totalSupply());
     }
 
-    function sellCredit(uint256 _sellAmount) public sellReq(_sellAmount) returns(bool) {
+    function sellCredit(
+        uint256 _sellAmount
+    ) public sellReq(_sellAmount) returns (bool) {
         toSell memory x = toSell(_sellAmount, msg.sender);
         toSellArr.push(x);
         return true;
     }
 
-    function buyCredit(uint256 _amount, address _buyFrom) public payable buyReq(_amount, _buyFrom) returns(bool) {
+    function buyCredit(
+        uint256 _amount,
+        address _buyFrom
+    ) public payable buyReq(_amount, _buyFrom) returns (bool) {
         //$ to seller
-        (bool success, ) = _buyFrom.call{value: _amount*creditRate()}("");
-        
+        (bool success, ) = _buyFrom.call{value: _amount * creditRate()}("");
+
         //CCT to buyer (90%)
         ERC20 CCT = ERC20(address(this));
 
         CCT.approve(address(this), _amount);
 
-        CCT.transferFrom(_buyFrom, msg.sender, (_amount*9)/10);
+        CCT.transferFrom(_buyFrom, msg.sender, (_amount * 9) / 10);
 
-        _burn(msg.sender, _amount/10);
+        _burn(msg.sender, _amount / 10);
 
         return success;
     }
 
-    function generateCredits(string memory _textHash, address _awardee) public generateCreditsMod(_textHash, _awardee) returns(bool){
-        _mint(_awardee, totalSupply()/100);
+    function generateCredits(
+        string memory _textHash,
+        address _awardee
+    ) public generateCreditsMod(_textHash, _awardee) returns (bool) {
+        _mint(_awardee, totalSupply() / 100);
         return true;
     }
-
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -8,6 +8,10 @@ module.exports = {
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
       accounts: [`0x${process.env.ACCOUNT_PRIVATE_KEY}`]
+    },
+    polygon: {
+      url: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.POLYGON_ALCHEMY_API}`,
+      accounts: [`0x${process.env.ACCOUNT_PRIVATE_KEY_POLYGON}`]
     }
   }
 };

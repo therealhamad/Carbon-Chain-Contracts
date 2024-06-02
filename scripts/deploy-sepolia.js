cb984e8ddddc1b650cb1030ed06c57ef4dc50801
Hamad Hussain <136684224+therealhamad@users.noreply.github.com>
2024-06-02 23:36:44 +0530

    Update deploy-sepolia.js

--- scripts/deploy-sepolia.js
+++ scripts/deploy-sepolia.js
@@ -3,6 +3,11 @@ async function main() {
   
     console.log("Deploying contracts with the account:", deployer.address);
     // console.log("Account balance:", (await deployer.getBalance()).toString());
+    // Chainlink Price Feed address for ETH/USD on Sepolia
+    const priceFeedAddressSepolia = "0xbA6D779Ebf3EADA6c805c29215751004dBDa46ef"; 
+
+    // Use the appropriate address based on the network you are deploying to
+    const priceFeedAddress = priceFeedAddressSepolia;
   
     const TokenBalanceChecker = await ethers.getContractFactory("CarbonChain");
     const tokenBalanceChecker = await TokenBalanceChecker.deploy(1000, 1500);

203604f28806e78c53c567b5e236070ec719b85a
Hamad Hussain <136684224+therealhamad@users.noreply.github.com>
2024-06-02 23:35:19 +0530

    Update deploy-polygon.js

--- scripts/deploy-polygon.js
+++ scripts/deploy-polygon.js
@@ -3,7 +3,11 @@ async function main() {
   
     console.log("Deploying contracts with the account:", deployer.address);
     // console.log("Account balance:", (await deployer.getBalance()).toString());
-  
+    // Chainlink Price Feed address for ETH/USD on Polygon
+    const priceFeedAddressPolygon = "0xF03c83b0fF20D5D5A13A15Fa8b0BA66385E9c91d"; 
+    // Use the appropriate address based on the network you are deploying to
+    const priceFeedAddress = priceFeedAddressPolygon; 
+    
     const TokenBalanceChecker = await ethers.getContractFactory("CarbonChain");
     const tokenBalanceChecker = await TokenBalanceChecker.deploy(1000, 1500);
   
